b4cb7c66e9a2a5190dfa817d29182bc645242fe5
Leonel Proia <leitoproia@gmail.com>
2025-06-24 10:15:39 -0300

    CreaTuLanding1+Proia Entrega 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
-
+import NavBar from "./components/NavBar"
+import ItemListContainer from './components/ItemListContainer'
 function App() {
+  const saludo = "Hola Mundo";
   return (
    <div>
-    <h1>Unidad 1</h1>
+    <NavBar />
+    <ItemListContainer saludo={"Bienvenido a MensClothes"} />
    </div>
   )
 }
